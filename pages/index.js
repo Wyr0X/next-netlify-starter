2cf2aad64ca6d98649573dc841e89b2246027077
Alexis Caraballo <alexiscaraballo96@gmail.com>
2023-09-29 17:20:29 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,8 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
-        <iframe style={{width: "100%", height: "500px"}} src="https://pacificodev.deyel.com/pages/ventaDigital?product=2&partner=3" />
+        <iframe style={{width: "1000px", height: "500px"}} src="https://pacifico.deyel.com/pages/ventaDigital?product=2&partner=3" />
+        <iframe style={{width: "1000px", height: "500px"}} src="https://pacifico.deyel.com/pages/ventaDigital?product=7&partner=3" />
       </main>
 
       <Footer />
