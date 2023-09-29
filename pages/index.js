6d1a29b96206250be486170af3a96fe9ee3df4b6
Alexis Caraballo <alexiscaraballo96@gmail.com>
2023-09-29 16:31:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <iframe src="https://pacifico.deyel.com/pages/ventaDigital?product=2&partner=3" />
       </main>
 
       <Footer />
